0d75733fcb87184438f0dcac0410c1ff6241fe02
wChenonly <wChenonly@qq.com>
2023-07-13 11:13:37 +0800

    🔨 refactor: remove server.listen fun

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,9 @@ export default function cliCopy(options: PluginOptions = {}): Plugin {
     name: 'vite-plugin-cli-copy',
     apply: 'serve',
     configureServer(server) {
-      const _listen = server.listen
-      server.listen = function () {
-        // eslint-disable-next-line prefer-rest-params
-        const isRestart = arguments[1] === true
-        if (!isRestart) {
-          server.httpServer?.on('listening', () => {
-            setTimeout(() => copy(server, options), 0)
-          })
-        }
-        // @ts-ignore
-        // eslint-disable-next-line prefer-rest-params
-        return _listen.apply(this, arguments)
-      }
+      server.httpServer?.on('listening', () => {
+        setTimeout(() => copy(server, options), 0)
+      })
     }
   }
 }
